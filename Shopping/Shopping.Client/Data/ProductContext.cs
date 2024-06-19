05ea3a8564fd16623725a23ff79be2faf8467386
Bharat <bbhutiya@knowyourcustomer.com>
2024-06-19 11:36:50 +0530

    initialization of web app

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product() { Name = "IPhone X", Description = "The phone with good camerga", ImageFile = "product-1.png", Price = 120000M, Category = "SmartPhone"},
+            new Product() { Name = "Samsung S20+", Description = "BigScreen Phone", ImageFile = "product-2.png", Price = 60000M, Category = "SmartPhone"},
+        };
+    }
+}

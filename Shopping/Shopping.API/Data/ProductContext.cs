7b8c9605b68b6d056b39d0305f803be732992b6b
BHARAT703 <bbhutiya703@gmail.com>
2024-06-19 16:17:41 +0530

    added support for mongo db

---
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using Shopping.API.Models;
+
+namespace Shopping.API.Data
+{
+    public class ProductContext
+    {
+        public IMongoCollection<Product> Products { get; }
+        public ProductContext(IConfiguration config)
+        {
+            var client = new MongoClient(config["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(config["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(config["DatabaseSettings:CollectionName"]);
+            SeedData(Products);
+        }
+
+        public static void SeedData(IMongoCollection<Product> products)
+        {
+            var productsExists = products.Find(m => true).Any();
+            if (!productsExists)
+            {
+                products.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        public static List<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>() {
+            new Product() { Name = "IPhone X", Description = "The phone with good camerga", ImageFile = "product-1.png", Price = 120000M, Category = "SmartPhone"},
+            new Product() { Name = "Samsung S20+", Description = "BigScreen Phone", ImageFile = "product-2.png", Price = 60000M, Category = "SmartPhone"},
+            new Product() { Name = "Oppo", Description = "chinese phone", ImageFile = "product-3.png", Price = 20000M, Category = "SmartPhone"},
+            new Product() { Name = "Vivo", Description = "chinese phone", ImageFile = "product-4.png", Price = 20000M, Category = "SmartPhone"},
+            };
+        }
+    }
+}
--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Shopping.Client.Data;
+using Newtonsoft.Json;
 using Shopping.Client.Models;
 using System.Diagnostics;
 
@@ -8,15 +8,21 @@ namespace Shopping.Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _client;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _client = clientFactory.CreateClient("ShoppingAPIClient");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(ProductContext.Products);
+            var response = await _client.GetAsync("/product");
+            var content = await response.Content.ReadAsStringAsync();
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+
+            return View(products);
         }
 
         public IActionResult Privacy()
